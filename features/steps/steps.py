816efec349047fcceb0a2338965909ae314e8ec3
Edwin Taquichiri <taquimon@gmail.com>
2025-07-02 19:34:38 -0400

    removed comment

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -44,7 +44,6 @@ def call_endpoints_with_json(context, method, action, endpoint_name):
     # add to list of projects for clean up
     if action == "create":
         append_to_feature_list(endpoint_name, context, response["body"]["id"])
-        # context.project_list.append(response["body"]["id"])
     LOGGER.debug(response)
     # store status code in context
     context.status_code = response["status_code"]
